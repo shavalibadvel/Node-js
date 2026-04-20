cd8540f64f4dcdc009601e6075ac3ebc812485d3
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-04-20 18:29:08 +0530

    changes

--- Practice/ProjectWithExpress/Routers/UserRouter.js
+++ Practice/ProjectWithExpress/Routers/UserRouter.js
@@ -5,9 +5,9 @@ const { passwordAuthMiddleware } = require("../Middleware/PasswordAuthMiddleware
 
 router.get("/getAllUsersData", passwordAuthMiddleware,getAllUsersDataHandler);
 
-//query params api
+//query params api for the app
 router.get("/getUsersByGender", passwordAuthMiddleware,getUsersByGenderHandler);
-// Url params api
+// Url params api for the app
 router.get("/getUserById/:id", passwordAuthMiddleware,getUserByIdHandler);
 router.get("/getUserByFirstName/:firstName",passwordAuthMiddleware,getUserByFirstNameHandler);
 
