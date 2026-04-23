aeb575401aafbb85c7abcd327cda628a8b786d35
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-04-23 21:47:21 +0530

    changes

--- Practice/ProjectWithExpress/index.js
+++ Practice/ProjectWithExpress/index.js
@@ -4,7 +4,7 @@ const UserRouter=require("./Routers/UserRouter");
 const HomeRouter=require("./Routers/HomeRouter");
 const app=express();
 const PORT=8069;
-
+// moved everything to the router for better code management and readability
 app.use("/",HomeRouter);
 app.use("/api/v1/users/",UserRouter);
 app.listen(PORT,()=>{
