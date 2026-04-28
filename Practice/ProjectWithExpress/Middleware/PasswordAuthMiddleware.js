bc1e31e767981a32ea171edea43fdc5d01a8ad54
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-04-28 20:27:18 +0530

    changes

--- Practice/ProjectWithExpress/Middleware/PasswordAuthMiddleware.js
+++ Practice/ProjectWithExpress/Middleware/PasswordAuthMiddleware.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const Server_Secrete_Password=process.env.Server_Secrete_Password;
 // We check password validation and if possible we can also check for the user role and permissions in this middleware for better security and access control of the app
-//ouigjigiugjkgeiugrijberg
+//ouigjigiugjkgeiugrijberg 
 function passwordAuthMiddleware(req,res,next){
-     const header=req.headers;
+    const header=req.headers;
     const authorizationHeader=header?.authorization;
     if(authorizationHeader!==Server_Secrete_Password){
         res.status(401).json({'message':"Unauthorized access. Invalid password. from header from middleware"});
