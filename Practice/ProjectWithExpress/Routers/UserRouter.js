156ae3a4d8380fab8576b141e029c71299b03b80
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-05-03 20:18:16 +0530

    changes

--- Practice/ProjectWithExpress/Routers/UserRouter.js
+++ Practice/ProjectWithExpress/Routers/UserRouter.js
@@ -7,7 +7,7 @@ router.get("/getAllUsersData", passwordAuthMiddleware,getAllUsersDataHandler);
 
 //query params api for the app
 router.get("/getUsersByGender", passwordAuthMiddleware,getUsersByGenderHandler);
-// Url params api for the app efkhegbjfn fuighoguijkagfughgi
+// Url params api for the app efkhegbjfn 
 router.get("/getUserById/:id", passwordAuthMiddleware,getUserByIdHandler);
 router.get("/getUserByFirstName/:firstName",passwordAuthMiddleware,getUserByFirstNameHandler);
 
