b61e085a7789a6096f1d6102c7bd23f4393062cd
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-05-04 17:58:24 +0530

    installed mongoose

--- Practice/ProjectWithExpress/index.js
+++ Practice/ProjectWithExpress/index.js
@@ -2,11 +2,21 @@
 const express=require("express");
 const UserRouter=require("./Routers/UserRouter");
 const HomeRouter=require("./Routers/HomeRouter");
+const { default: mongoose } = require("mongoose");
 const app=express();
 const PORT=8069;
+const uri="mongodb://localhost:27017/" //server
+const dbName="Node-Crio" //database name
+mongoose.connect(uri+dbName).then(()=>{
+    console.log("Connected to MongoDB successfully");
+}).catch((err)=>{
+    console.error("Error connecting to MongoDB:", err);
+});
+
 // moved everything to the router for better code management and readability urikgnougrikjdsn
 app.use("/",HomeRouter);
 app.use("/api/v1/users/",UserRouter);
+
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT}`);
 });
