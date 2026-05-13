8c8bef093bd4ed24384302d0469ea49e36331b52
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-05-13 19:18:05 +0530

    comments

--- Practice/ProjectWithExpress/Model/UserModel.js
+++ Practice/ProjectWithExpress/Model/UserModel.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
     //     type: Date,
     //     default: Date.now
     // }
-    //rguijkfgyihbjwaygs
+    //just some comments to check the git status and commit changes
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
