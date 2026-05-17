678f10fea95ca3902571eb9ae2fcf11e284e00b7
shavalibadvel <55553298+shavalibadvel@users.noreply.github.com>
2026-05-17 19:51:42 +0530

    changes

--- Practice/ProjectWithExpress/Routers/BlogsRouter.js
+++ Practice/ProjectWithExpress/Routers/BlogsRouter.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const {getAllBlogs,getBlogById,createBlog,updateBlogById,deleteBlogById}=require("../Controller/BlogsController");
 const router=express.Router();
-//some comments to check the git status and commit changes uogjklenrou
+//some comments to check the git status and commit changes uogjklenrou iygurhjkqwewgeyh
 router.post("/createBlog",createBlog);
 router.get("/getAllBlogs",getAllBlogs);
 router.get("/getBlogById/:id",getBlogById);
